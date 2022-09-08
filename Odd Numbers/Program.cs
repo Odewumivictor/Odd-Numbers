e53bc3f11729df2e37f7e37cea1cdff23ce613a8
odewumivictor <vickidubi1@gmail.com>
2022-09-08 10:23:33 +0100

    Add project files.

---
+++ Odd Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Numbers
+{
+    public class SumOfOddNumbers
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Odd numbers from 0 to 100");
+            for (int n = 1; n < (100 + 1); n++)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.WriteLine(n);
+                }
+               
+            }
+           
+        }
+    }
+}
